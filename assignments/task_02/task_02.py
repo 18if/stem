e24f96a8c6a58bd83f9edec18e35189e5c9db86d
student <student>
2018-02-15 11:54:56 -0500

    i tried and failed

--- assignments/task_02/task_02.py
+++ assignments/task_02/task_02.py
@@ -1,9 +1,12 @@
 # Write a function called total that takes a filename as its only argument and returns the sum of the numbers in that file.
 def total(filename):
-    file
+    with open(filename) as f:
+        in f f.int(f)
+        (f + f)/2 
     return
 # You may assume that the file is formatted in such a way that each number is an integer and occupies its own line.
 # You may not assume you know how many numbers are in the file.
 
 # The file numbers.txt in this directory follows these guidelines and should add up to 4753817
 # After you've written your function, you can run test.py to ensure your function works on other files as well.
+#take int. and add stuff
